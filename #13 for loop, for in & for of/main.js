12dc323bde115159c6899aaacabadb85334b0f2a
sajedur-me <sajedur.me@gmail.com>
2026-04-24 11:45:15 +0600

    For loop, in, of add & testing

---
+++ #13 for loop, for in & for of/main.js
@@ -0,0 +1,26 @@
+// কোডের ব্যাখ্যা: 
+
+for(let i=0; i<=5; i++) {
+    console.log(i);
+}
+
+// কোডের ব্যাখ্যা: 
+const numbers = [1, 2, 3];
+for(let i=0; i<numbers.length; i++) {
+    console.log(numbers[i]);
+}
+for(let number of numbers) {
+    console.log(number);
+}
+
+
+const myString = "Hello";
+for(let element of myString) {
+    console.log(element);
+}
+for(let element in myString) {
+    console.log(element);
+}
+
+// বাস্তব কিছু ব্যবহার ও বিস্তারিত: 
+
